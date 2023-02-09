596f003a9c71a63a0405f4adfe909bc07fbd6e68
Harold-Ereama <ogemdiereama@gmail.com>
2023-02-09 15:15:56 +0100

    index page overhaul

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://.com</homePageLink>
+		<homePageLink>https://plutontech.org</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://plutontech.org/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://plutontech.org/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://plutontech.org/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://plutontech.org/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://plutontech.org/wp-json/" />
 			</apis>
 	</service>
 </rsd>
